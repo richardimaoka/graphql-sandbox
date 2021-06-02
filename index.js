85854d19079bb6bad3a5ec0feb11a9c4711fcc38
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-06-02 21:28:54 +0900

    scalar type

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const { ApolloServer, gql } = require("apollo-server");
 const fs = require("fs");
+const { GraphQLScalarType, Kind } = require("graphql");
+
+const dateScalar = new GraphQLScalarType({
+  name: "Date",
+  description: "Date custom scalar type",
+  serialize(value) {
+    return value.getTime(); // Convert outgoing Date to integer for JSON
+  },
+  parseValue(value) {
+    return new Date(value); // Convert incoming integer to Date
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
+    }
+    return null; // Invalid hard-coded value (not an integer)
+  },
+});
 
 const typeDefs = gql`
   ${fs.readFileSync(__dirname.concat("/coursera.gql"), "utf8")}
