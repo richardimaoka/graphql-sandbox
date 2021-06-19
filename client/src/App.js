3c5102789043c58737f9e31c0abac21c1fe63aa1
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-06-20 06:54:17 +0900

    apollo client in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+import { useEffect } from "react";
+
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache(),
+});
+
+// const client = ...
 
 function App() {
+  const a = "Learn react for greater good";
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          query GetRates {
+            rates(currency: "USD") {
+              currency
+            }
+          }
+        `,
+      })
+      .then((result) => console.log(result));
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p onClick={() => console.log("clicked")}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -15,7 +45,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {a}
         </a>
       </header>
     </div>
