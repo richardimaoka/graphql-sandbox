234b6945f0a5d89b65b965960a7a2b93a92983d5
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-06-23 20:40:21 +0900

    remove react log img

--- client/src/App.js
+++ client/src/App.js
@@ -31,18 +31,6 @@ function App() {
     <div className="App">
       <ApolloProvider client={client}>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={() => console.log("clicked")}>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {a}
-          </a>
           <Child></Child>
         </header>
       </ApolloProvider>
